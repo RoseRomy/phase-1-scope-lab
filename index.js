032e9813721fdeed181ca6e74fd2eee795e8d069
+ <RoseKamau@Romys-Macboook-Air.local>
2025-03-05 10:22:49 +0300

    Addes index.js

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// Write your solution in this file!
+// Declare customerName in global scope
+var customerName = 'bob';
+function upperCaseCustomerName() {
+    customerName = customerName.toUpperCase();
+}
+// Declare bestCustomer in global scope
+var bestCustomer;
+function setBestCustomer() {
+    bestCustomer = 'not bob';
+}
+function overwritesBestCustomer() {
+    bestCustomer = 'maybe bob';
+}
+const leastFavoriteCustomer = 'someone';
+function changeLeastFavoriteCustomer() {
+    leastFavoriteCustomer = 'new person';
+}
+function overwriteBestCustomer() {
+    bestCustomer = 'maybe bob';
+}
